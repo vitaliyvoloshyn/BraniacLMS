034d4a5d4f44278de0eda622a748bf78990a1aac
Vitaliy Voloshyn <vitaliy.srt1985@gmail.com>
2022-05-09 13:25:57 +0300

    'task_done'

---
+++ mainapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+
+app_name = MainappConfig.name
+
+urlpatterns = [
+    path("", views.MainPageView.as_view(), name='index'),
+    path("news/", views.NewsPageView.as_view(), name='news'),
+    path("courses/", views.CoursesPageView.as_view(),name='courses'),
+    path("contacts/", views.ContactsPageView.as_view(), name='contacts'),
+    path("doc_site/", views.DocSitePageView.as_view(), name='docsite'),
+    path("login/", views.LoginPageView.as_view(), name='login'),
+]
