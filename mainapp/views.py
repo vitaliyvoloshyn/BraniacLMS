0ca9eaee454f2cc65051760143c67fd3cd9ec4e0
vitaliyvoloshyn <vitaliy.srt1945@gmail.com>
2022-05-25 12:03:14 +0300

    Созданы модели новостей, курсов, переподавателей,уроков..Созданы и применены файлы миграции..Модель Новости заполена несколькими записями..Отредактированы вьюхи страницы News

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,30 +4,33 @@ from django.http import HttpRequest
 from django.views.generic import TemplateView, ListView, DetailView
 import json
 
+from mainapp.models import News
+
 
 class MainPageView(TemplateView):
     template_name = "mainapp/base.html"
 
 
 class NewsPageView(ListView):
+    model = News
     template_name = "mainapp/news.html"
     paginate_by = 3
     
     def get_queryset(self):
-        
-        with open('mainapp/templates/mainapp/news.json', 'r') as f:
-            self.queryset = json.load(f)
-        return self.queryset
+        queryset = super().get_queryset()
+        queryset = queryset.filter(deleted=False).order_by('-created')
+        return queryset
 
 
 class ShowNews(DetailView):
     template_name = "mainapp/selected_news.html"
     pk_url_kwarg = 'news_id'
+    model = News
 
-    def get_object(self):
-        with open('mainapp/templates/mainapp/news.json', 'r') as f:
-            self.queryset = json.load(f)
-        return self.queryset[self.kwargs.get(self.pk_url_kwarg) - 1]
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(deleted=False).order_by('-created')
+        return queryset
 
 
 class CoursesPageView(TemplateView):
